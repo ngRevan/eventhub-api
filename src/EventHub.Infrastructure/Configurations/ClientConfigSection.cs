d4661c31e395b09d1400fa3ddc5143a89abcb551
Daniel Els <Daniel.Els@gmz.migros.ch>
2020-03-03 11:43:36 +0100

    Initial project setup

---
+++ src/EventHub.Infrastructure/Configurations/ClientConfigSection.cs
@@ -0,0 +1,14 @@
+﻿namespace EventHub.Infrastructure.Configurations
+{
+    public class ClientConfigSection
+    {
+        public const string SectionName = "ClientConfig";
+
+        public class AppSettingsSection
+        {
+            public string Environment { get; set; }
+
+            public string ApiUrl { get; set; }
+        }
+    }
+}
